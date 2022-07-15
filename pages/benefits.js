d7481b0db82d6e16394bd083065010612e9ea919
Nicolas <pinier74nicolas@gmail.com>
2022-07-15 20:44:50 +0200

    minor changement in global css about h1 size

--- pages/benefits.js
+++ pages/benefits.js
@@ -3,9 +3,22 @@ import fs from 'fs';
 import matter from 'gray-matter';
 
 const Benefits = ( {rates} ) => {
+  console.log(rates);
+
   return (
     <div>
       <h1>Tarifs et Prestations</h1>
+      {
+        rates.map((rate, i) => {
+          return (
+            <div key={i}>
+              <h5>{rate.label}</h5>
+              <h5>{rate.description}</h5>
+              <h5>{rate.prix}€</h5>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
@@ -16,14 +29,15 @@ export const getStaticProps = async () => {
 
   // Get the front matter and slug (the filename without .md) of all files
   const rates = filesInPhotos.map(filename => {
-    const file = fs.readFileSync(`./Tarifs/${filename}`, 'utf8')
-    const matterData = matter(file)
 
-    return {
-      ...matterData.data, // matterData.data contains front matter
-      slug: filename.slice(0, filename.indexOf('.'))
-    }
-  })
+      const file = fs.readFileSync(`./Tarifs/${filename}`, 'utf8')
+      const matterData = matter(file)
+      
+      return {
+        ...matterData.data, // matterData.data contains front matter
+        slug: filename.slice(1, filename.indexOf('.'))
+      }
+    })
 
   return {
     props: {
